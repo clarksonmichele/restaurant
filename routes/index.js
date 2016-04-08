70794de01f2a403b63bdaf2ac435097b2f7f4a26
micheleclarkson <micheleclarkson@rogers.com>
2016-04-07 20:45:17 -0400

    Added css and images
    Still have to figure out how to get the logo in the nav on private pages

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 
-// home page
+//public menu page
 router.get('/', function(req, res, next) {
     res.render('index', {
         title: 'Basilico',
